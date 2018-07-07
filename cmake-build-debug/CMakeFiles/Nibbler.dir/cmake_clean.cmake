33de617d7372651b0dca481cca22157c8f9d5f88
vkozlov <vadim.k.394010@gmail.com>
2018-07-07 15:28:35 +0300

    Updated submodule

---
+++ cmake-build-debug/CMakeFiles/Nibbler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nibbler.dir/nibbler_glfw/src/nibbler_glfw.cpp.o"
+  "CMakeFiles/Nibbler.dir/nibbler_glfw/src/Shader.cpp.o"
+  "CMakeFiles/Nibbler.dir/nibbler_glfw/src/Texture.cpp.o"
+  "Nibbler.pdb"
+  "Nibbler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nibbler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
